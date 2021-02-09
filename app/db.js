3f0e42f59f2e3af18da09286a4fe6f2a661c7e42
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-02-09 22:19:33 +0100

    danother config

--- app/db.js
+++ app/db.js
@@ -14,7 +14,7 @@ const sequelize = new Sequelize(process.env.DATABASE_URL,{
     dialect:  'postgres',
     protocol: 'postgres',
  
-    logging:  debug, //false
+    logging:  false, //false
 
     dialectOptions: {
         ssl: true

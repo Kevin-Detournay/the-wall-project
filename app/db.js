acef951ba259bf8d779b64e07f6cd43f71232efe
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-02-09 21:47:03 +0100

    test 5

--- app/db.js
+++ app/db.js
@@ -4,12 +4,13 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
     define: {
         // Afin de dire à sequelize que l'on utilise une convention de nommage en snake_case, on active l'option undescored
         underscored: true,
-
+        
         // Afin de modifier le nom de la propriété des champs en sorti de requête sequelize on précise des correspondances
         createdAt: 'created_at',
         updatedAt : 'updated_at'
     },
     logging:false,
+    ssl:false,
 });
 
 module.exports = sequelize;

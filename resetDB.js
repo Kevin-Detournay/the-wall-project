bc3676457e9a4513ecd512345167081b41bc4edc
Kevin-Detournay <kevindetournay@hotmail.fr>
2021-02-10 17:16:04 +0100

    fin de session

--- resetDB.js
+++
@@ -1,4 +0,0 @@
-const execSync = require('child_process').execSync;
-// import { execSync } from 'child_process';  // replace ^ if using ES modules
-const output = execSync('heroku pg:backups:restore b001 --confirm  dry-sands-45238', { encoding: 'utf-8' });  // the default is 'buffer'
-console.log('Output was:\n', output);
